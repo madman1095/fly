7a2db8d340533823563b8f41cb1afaf2d2e6257b
madman1095 <so2softplut@gmail.com>
2025-10-28 01:30:49 +0300

    Update fly.lua

--- fly.lua
+++ fly.lua
@@ -381,6 +381,31 @@ local Button = TeleportTab:CreateButton({
    Name = "Random Players",
    Callback = function()
 
-		
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local TeleportService = game:GetService("TeleportService")
+
+local function teleportToRandomPlayer()
+    local players = Players:GetPlayers()
+    if #players < 2 then
+        warn("Not enough players to teleport")
+        return
+    end
+
+    local randomPlayer
+    repeat
+        randomPlayer = players[math.random(1, #players)]
+    until randomPlayer ~= LocalPlayer
+
+    local originalPosition = LocalPlayer.Character.HumanoidRootPart.CFrame
+    LocalPlayer.Character.HumanoidRootPart.CFrame = randomPlayer.Character.HumanoidRootPart.CFrame
+
+    wait(1000)  -- The time after which we returned to our starting position.
+
+    LocalPlayer.Character.HumanoidRootPart.CFrame = originalPosition
+end
+
+teleportToRandomPlayer()		
+		end
    end,
 })
